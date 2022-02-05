33dbbdca0d9f11a7a9e58d64dd02370c0040a373
zahidhasan23 <zahid0911202@gmail.com>
2022-02-05 10:41:13 +0600

    added variable

---
+++ variable.js
@@ -0,0 +1,9 @@
+var price = 21;
+var age =23;
+// amare sriler temperature
+var temperature =37;
+var phonePrice =24500;
+// My destop price
+var prics =33000;
+// programming hero price 5500
+var hero =4500;
